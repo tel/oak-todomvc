c875e2f8c41fe75029ece2b928b7687e3992fedb
Joseph Abrahamson <me@jspha.com>
2016-04-20 11:28:13 -0400

    update project metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject oak-todomvc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "TodoMVC using Oak components"
+  :url "http://github.com/tel/oak-todomvc"
+  :license {:name "BSD3"
+            :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.8.40"]
                  [oak "0.1.0-SNAPSHOT"]
